aa378c1f00a67ddc2c6eea06c050b70e371e3782
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-11-21 11:02:24 +0900

    WIP

--- src/main.c
+++ src/main.c
@@ -79,7 +79,19 @@ int main(int argc, char *argv[]) {
   // int resb = bind(fd_send, (struct sockaddr *) &sout, sizeof(struct sockaddr_in));
   int resb = connect(fd_send, (struct sockaddr*)&sout, sizeof(struct sockaddr_in));
   // assert(resb != -1);
-  printf("connect result: %d\n", resb);
+  printf("Connecting to %s:%d\n", out_addr, out_port);
+  if(resb < 0){
+    printf("Failed to connect, result: %d\n", resb);
+
+    // close the UDP out socket
+    #ifdef _WIN32
+      closesocket(fd_send);
+    #else
+      close(fd_send);
+    #endif
+
+    exit(1);
+  }
 
   char buffer[2048]; // declare a 2Kb buffer to read packet data into
 
@@ -99,69 +111,70 @@ int main(int argc, char *argv[]) {
 
   printf("done.\n");
 
-  // close the UDP socket
+  // register the SIGINT handler (Ctrl+C)
+  signal(SIGINT, &sigintHandler);
+
+  // // open a socket to listen for datagrams (i.e. UDP packets) on port 9000
+  // const int fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+//   // set the socket to non-blocking
+// #ifndef _WIN32
+//   fcntl(fd, F_SETFL, O_NONBLOCK); 
+// #else
+//   u_long mode = 1; // non blocking
+//   ioctlsocket(fd, FIONBIO, &mode);
+// #endif
+
+  // struct sockaddr_in sin;
+  // sin.sin_family = AF_INET;
+  // sin.sin_port = htons(9000);
+  // sin.sin_addr.s_addr = INADDR_ANY;
+  // bind(fd, (struct sockaddr *) &sin, sizeof(struct sockaddr_in));
+  // printf("tinyosc is now listening on port 9000.\n");
+
+  printf("Press Ctrl+C to stop.\n");
+
+  while (keepRunning) {
+    // fd_set readSet;
+    // FD_ZERO(&readSet);
+    // FD_SET(fd, &readSet);
+    // struct timeval timeout = {1, 0}; // select times out after 1 second
+    // if (select(fd+1, &readSet, NULL, NULL, &timeout) > 0) {
+    //   struct sockaddr sa; // can be safely cast to sockaddr_in
+    //   socklen_t sa_len = sizeof(struct sockaddr_in);
+    //   int len = 0;
+    //   while ((len = (int) recvfrom(fd, buffer, sizeof(buffer), 0, &sa, &sa_len)) > 0) {
+    //     if (tosc_isBundle(buffer)) {
+    //       tosc_bundle bundle;
+    //       tosc_parseBundle(&bundle, buffer, len);
+    //       const uint64_t timetag = tosc_getTimetag(&bundle);
+    //       tosc_message osc;
+    //       while (tosc_getNextMessage(&bundle, &osc)) {
+    //         tosc_printMessage(&osc);
+    //       }
+    //     } else {
+    //       tosc_message osc;
+    //       tosc_parseMessage(&osc, buffer, len);
+    //       tosc_printMessage(&osc);
+    //     }
+    //   }
+    // }
+  }
+
+  // close the UDP out socket
 #ifdef _WIN32
   closesocket(fd_send);
 #else
   close(fd_send);
 #endif
 
-  // register the SIGINT handler (Ctrl+C)
-  signal(SIGINT, &sigintHandler);
-
-  // open a socket to listen for datagrams (i.e. UDP packets) on port 9000
-  const int fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-  // set the socket to non-blocking
-#ifndef _WIN32
-  fcntl(fd, F_SETFL, O_NONBLOCK); 
-#else
-  u_long mode = 1; // non blocking
-  ioctlsocket(fd, FIONBIO, &mode);
-#endif
-
-  struct sockaddr_in sin;
-  sin.sin_family = AF_INET;
-  sin.sin_port = htons(9000);
-  sin.sin_addr.s_addr = INADDR_ANY;
-  bind(fd, (struct sockaddr *) &sin, sizeof(struct sockaddr_in));
-  printf("tinyosc is now listening on port 9000.\n");
-  printf("Press Ctrl+C to stop.\n");
-
-  while (keepRunning) {
-    fd_set readSet;
-    FD_ZERO(&readSet);
-    FD_SET(fd, &readSet);
-    struct timeval timeout = {1, 0}; // select times out after 1 second
-    if (select(fd+1, &readSet, NULL, NULL, &timeout) > 0) {
-      struct sockaddr sa; // can be safely cast to sockaddr_in
-      socklen_t sa_len = sizeof(struct sockaddr_in);
-      int len = 0;
-      while ((len = (int) recvfrom(fd, buffer, sizeof(buffer), 0, &sa, &sa_len)) > 0) {
-        if (tosc_isBundle(buffer)) {
-          tosc_bundle bundle;
-          tosc_parseBundle(&bundle, buffer, len);
-          const uint64_t timetag = tosc_getTimetag(&bundle);
-          tosc_message osc;
-          while (tosc_getNextMessage(&bundle, &osc)) {
-            tosc_printMessage(&osc);
-          }
-        } else {
-          tosc_message osc;
-          tosc_parseMessage(&osc, buffer, len);
-          tosc_printMessage(&osc);
-        }
-      }
-    }
-  }
-
-  // close the UDP socket
-#ifdef _WIN32
-  closesocket(fd);
-  WSACleanup();
-#else
-  close(fd);
-#endif
+//   // close the UDP in socket
+// #ifdef _WIN32
+//   closesocket(fd);
+//   WSACleanup();
+// #else
+//   close(fd);
+// #endif
 
   return 0;
 }
