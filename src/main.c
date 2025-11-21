5cd7adecd4cf4a93a9e775fe5ec89789aa31a9ac
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-11-21 11:15:40 +0900

    feat: implement osc sender

--- src/main.c
+++ src/main.c
@@ -1,18 +1,3 @@
-/**
- * Copyright (c) 2015, Martin Roth (mhroth@gmail.com)
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
- * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
- * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
- * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- * PERFORMANCE OF THIS SOFTWARE.
- */
 #ifdef _WIN32
 #include <WinSock2.h>
 #include <WS2tcpip.h>
@@ -33,12 +18,25 @@
 #include <stdlib.h>
 
 #include "tinyosc.h"
+#include "manymouse.h"
 
 static volatile bool keepRunning = true;
 
 // handle Ctrl+C
 static void sigintHandler(int x) {
   keepRunning = false;
+}
+
+int osc_send(SOCKET* fd, unsigned int device, unsigned int axis, int delta){
+  char buffer[2048]; // declare a 2Kb buffer to read packet data into
+
+  int len = 0;
+  len = tosc_writeMessage(buffer, sizeof(buffer), "/manymouse/relmotion", "iii", device, axis, delta);
+
+  // tosc_printOscBuffer(buffer, len); // for debug
+
+  int res = send(fd, buffer, len, 0);
+  return res;
 }
 
 /**
@@ -56,7 +54,7 @@ int main(int argc, char *argv[]) {
 #endif
 
   // open a socket to send for datagrams (i.e. UDP packets) on port 9001
-  int fd_send = socket(AF_INET, SOCK_DGRAM, 0);
+  SOCKET* fd_send = socket(AF_INET, SOCK_DGRAM, 0);
 
   // set the socket to non-blocking
 #ifndef _WIN32
@@ -76,11 +74,11 @@ int main(int argc, char *argv[]) {
   hostinfo = gethostbyname(out_addr);
   memcpy(&sout.sin_addr, hostinfo->h_addr_list[0], hostinfo->h_length);
   sout.sin_port = htons(out_port);
-  // sout.sin_addr.s_addr = "localhost";
-  // int resb = bind(fd_send, (struct sockaddr *) &sout, sizeof(struct sockaddr_in));
+
   int resb = connect(fd_send, (struct sockaddr*)&sout, sizeof(struct sockaddr_in));
-  // assert(resb != -1);
+
   printf("Connecting to %s:%d\n", out_addr, out_port);
+
   if(resb < 0){
     printf("Failed to connect, result: %d\n", resb);
 
@@ -95,72 +93,51 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  char buffer[2048]; // declare a 2Kb buffer to read packet data into
+  // char buffer[2048]; // declare a 2Kb buffer to read packet data into
 
-  printf("Starting write tests:\n");
-  int len = 0;
-  // char blob[8] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
-  // len = tosc_writeMessage(buffer, sizeof(buffer), "/address", "fsibTFNI",
-  //     1.0f, "hello world", -1, sizeof(blob), blob);
-  // char blob[8] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
-  len = tosc_writeMessage(buffer, sizeof(buffer), "/hello", "fi", 1.0f, 2);
-  tosc_printOscBuffer(buffer, len);
+  // printf("Starting write tests:\n");
+  // int len = 0;
+  // // char blob[8] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+  // // len = tosc_writeMessage(buffer, sizeof(buffer), "/address", "fsibTFNI",
+  // //     1.0f, "hello world", -1, sizeof(blob), blob);
+  // // char blob[8] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+  // len = tosc_writeMessage(buffer, sizeof(buffer), "/hello", "fi", 1.0f, 2);
+  // tosc_printOscBuffer(buffer, len);
 
-  printf("Sending to %s:%d\n", out_addr, out_port);
+  // printf("Sending to %s:%d\n", out_addr, out_port);
 
-  int res = send(fd_send, buffer, len, 0);
-  printf("total bytes sent: %d\n", res);
+  // int res = send(fd_send, buffer, len, 0);
+  // printf("total bytes sent: %d\n", res);
 
-  printf("done.\n");
+  // printf("done.\n");
+
+  // many mouse init
+  unsigned int device_count = ManyMouse_Init();
+  if(device_count <= 0){
+    printf("many mouse init failed\n");
+    return 1;
+  }
+
+  printf("many mouse device count: %d\n", device_count);
 
   // register the SIGINT handler (Ctrl+C)
   signal(SIGINT, &sigintHandler);
 
-  // // open a socket to listen for datagrams (i.e. UDP packets) on port 9000
-  // const int fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-//   // set the socket to non-blocking
-// #ifndef _WIN32
-//   fcntl(fd, F_SETFL, O_NONBLOCK); 
-// #else
-//   u_long mode = 1; // non blocking
-//   ioctlsocket(fd, FIONBIO, &mode);
-// #endif
-
-  // struct sockaddr_in sin;
-  // sin.sin_family = AF_INET;
-  // sin.sin_port = htons(9000);
-  // sin.sin_addr.s_addr = INADDR_ANY;
-  // bind(fd, (struct sockaddr *) &sin, sizeof(struct sockaddr_in));
-  // printf("tinyosc is now listening on port 9000.\n");
-
   printf("Press Ctrl+C to stop.\n");
 
+  ManyMouseEvent m;
+  char buffer[2048]; // declare a 2Kb buffer to read packet data into
+
   while (keepRunning) {
-    // fd_set readSet;
-    // FD_ZERO(&readSet);
-    // FD_SET(fd, &readSet);
-    // struct timeval timeout = {1, 0}; // select times out after 1 second
-    // if (select(fd+1, &readSet, NULL, NULL, &timeout) > 0) {
-    //   struct sockaddr sa; // can be safely cast to sockaddr_in
-    //   socklen_t sa_len = sizeof(struct sockaddr_in);
-    //   int len = 0;
-    //   while ((len = (int) recvfrom(fd, buffer, sizeof(buffer), 0, &sa, &sa_len)) > 0) {
-    //     if (tosc_isBundle(buffer)) {
-    //       tosc_bundle bundle;
-    //       tosc_parseBundle(&bundle, buffer, len);
-    //       const uint64_t timetag = tosc_getTimetag(&bundle);
-    //       tosc_message osc;
-    //       while (tosc_getNextMessage(&bundle, &osc)) {
-    //         tosc_printMessage(&osc);
-    //       }
-    //     } else {
-    //       tosc_message osc;
-    //       tosc_parseMessage(&osc, buffer, len);
-    //       tosc_printMessage(&osc);
-    //     }
-    //   }
-    // }
+    while(ManyMouse_PollEvent(&m)){
+      if(m.type == MANYMOUSE_EVENT_RELMOTION){
+        unsigned int device = m.device;
+        unsigned int axis = m.item;
+        int delta = m.value;
+
+        osc_send(fd_send, device, axis, delta);
+      }
+    }
   }
 
   // close the UDP out socket
